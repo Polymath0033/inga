11e83b3ce819916ed90de0c902a4f72c6fe0804e
Yousouph Olosan <depolymathyousouph@gmail.com>
2025-01-27 19:54:02 +0100

    first commit

---
+++ src/main/java/com/polymath/inga/ingaapi/IngaapiApplication.java
@@ -0,0 +1,13 @@
+package com.polymath.inga.ingaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polymath/inga/ingaapi/IngaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polymath.inga.ingaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
